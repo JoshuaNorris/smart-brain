a10c8e30c9ff5c57aa082ce7afa75ca661baa657
Joshua Norris <joshua.norris52@gmail.com>
2024-04-30 15:35:40 -0500

    fiking bugs

--- src/App.js
+++ src/App.js
@@ -87,44 +87,18 @@ function App() {
 
 
   const getAPI = (imageURL) => {
-
-    const PAT = 'e53f23ed7b07471caf0bec6692c20a1d';
-    const USER_ID = 'norris52';       
-    const APP_ID = 'smart-brain';
-    const MODEL_ID = 'face-detection';
-    const IMAGE_URL = imageURL;
-    const raw = JSON.stringify({
-      "user_app_id": {
-        "user_id": USER_ID,
-        "app_id": APP_ID
-      },
-      "inputs": [
-        {
-          "data": {
-            "image": {
-              "url": IMAGE_URL
-            }
-          }
-        }
-      ]
-    });
-      
-
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Authorization': 'Key ' + PAT
-      },
-      body: raw
-    };
-  
-
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+    setImageURL(imageURL);
+      fetch('https://dumb-brain-ff26d6635c24.herokuapp.com/imageurl', {
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            input: input
+          })
+        })
     .then(response => response.json())
-    .then(result => { 
-      if (result) {
-        fetch('http://localhost:3000/image', {
+    .then(response => { 
+      if (response) {
+        fetch('https://dumb-brain-ff26d6635c24.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -141,10 +115,10 @@ function App() {
               joined: user.joined
             })
           })
-          .catch(err => console.log(err));
+          .catch(err => console.log("Got this error after putting the image and trying to set user. " + err));
+
       }
-      const box = calculateFaceLocation(result);
-      displayFaceBox(box);
+      displayFaceBox(calculateFaceLocation(response));
     })
     .catch(error => console.log('error', error));
   }
@@ -171,5 +145,30 @@ function App() {
 // In this section, we set the user authentication, user and app ID, model details, and the URL
 // of the image we want as an input. Change these strings to run your own example.
 //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // const regions = result.outputs[0].data.regions;
+
+
+        // regions.forEach(region => {
+        //     // Accessing and rounding the bounding box values
+        //     const boundingBox = region.region_info.bounding_box;
+        //     const topRow = boundingBox.top_row.toFixed(3);
+        //     const leftCol = boundingBox.left_col.toFixed(3);
+        //     const bottomRow = boundingBox.bottom_row.toFixed(3);
+        //     const rightCol = boundingBox.right_col.toFixed(3);
+        //     console.log(topRow);
+
+        //     region.data.concepts.forEach(concept => {
+        //         // Accessing and rounding the concept value
+        //         const name = concept.name;
+        //         const value = concept.value.toFixed(4);
+
+        //         console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+                
+        //     });
+        // });
+
+    
+
 
 export default App;
